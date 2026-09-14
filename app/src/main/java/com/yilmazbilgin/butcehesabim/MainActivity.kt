f5a3d4563ba4b31215d0942dc24dfd39cfcf9b90
yilmazbilgin <ylmzbsqn@gmail.com>
2026-09-14 21:47:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yilmazbilgin/butcehesabim/MainActivity.kt
+++ app/src/main/java/com/yilmazbilgin/butcehesabim/MainActivity.kt
@@ -42,7 +42,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
